068bcba150123e69e002cfa7dcc6a2bf98cf4a5b
Andy Kim <w9h0b@ugrad.cs.ubc.ca>
2020-08-01 13:12:51 -0700

    Health UI tentative

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour{
+    public Slider slider;
+
+    public void SetMaxHealth(float val)
+    {
+        slider.maxValue = val;
+        slider.value = val;
+    }
+    
+    public void SetHealth(float val)
+    {
+        slider.value = val;
+    }
+}
