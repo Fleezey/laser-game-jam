6642507e2461f3eb94e8b435e599661cf4d977ce
Andy Kim <w9h0b@ugrad.cs.ubc.ca>
2020-08-01 20:09:34 -0700

    adding wave UI

---
+++ Assets/Scripts/UI/WaveUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Wave;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveUI : MonoBehaviour{
+    public Text waveText;
+
+    private void Update()
+    {
+        waveText.text = "Current Wave: " + WaveManager.Instance.GetScore();
+    }
+}
