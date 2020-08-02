c62eeecd35f908de05e90b2c45f1245a9738c3b6
Andy Kim <w9h0b@ugrad.cs.ubc.ca>
2020-08-01 22:39:39 -0700

    Mainmenu

---
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Game.Audio;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public void ChangeMasterVolume(float percent)
+	{
+		AudioManager.Instance.SetVolume(percent, AudioManager.AudioChannel.Master);
+	}
+	
+	public void ChangeSFXVolume(float percent)
+	{
+		AudioManager.Instance.SetVolume(percent, AudioManager.AudioChannel.Sfx);
+	}
+	
+	public void ChangeMusicVolume(float percent)
+	{
+		AudioManager.Instance.SetVolume(percent, AudioManager.AudioChannel.Music);
+	}
+}
